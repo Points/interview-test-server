83b84c83e980f00e3e7f5501b4e49d09de9a7d17
Artur Levchenko <artur.levchenko@points.com>
2021-11-18 09:55:38 -0500

    removed trailing / from the routes

--- api/tax_calculator/routes.py
+++ api/tax_calculator/routes.py
@@ -8,14 +8,14 @@ def tax_calculator_instructions():
     return render_template('tax-calculator.html')
 
 
-@app.route('/tax-calculator/brackets/')
+@app.route('/tax-calculator/brackets')
 def default_brackets():
     return jsonify({
         'tax_brackets': controllers.get_reliable_brackets()
     })
 
 
-@app.route('/tax-calculator/brackets/<tax_year>/')
+@app.route('/tax-calculator/brackets/<tax_year>')
 def tax_year_brackets(tax_year):
     return jsonify({
         'tax_brackets': controllers.get_unreliable_brackets(tax_year)
