21d240d15804891ca8d02a319c4b71968ebf9865
Jamie Weatherby <jamie.weatherby@points.com>
2022-02-25 10:18:13 -0500

    Add tests to ensure api routes work for both assignments
    Add newlines

---
+++ conftest.py
@@ -0,0 +1,11 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+
+    with app.app_context():
+        with app.test_client() as client:
+            yield client
+            
