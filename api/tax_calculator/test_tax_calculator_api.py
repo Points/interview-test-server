81bc71ce8e05a0168bccb2d492df14e85d3a79bb
Jamie Weatherby <jamie.weatherby@points.com>
2022-02-25 10:18:13 -0500

    Add tests to ensure api routes work for both assignments
    Add newlines

---
+++ api/tax_calculator/test_tax_calculator_api.py
@@ -0,0 +1,21 @@
+import os
+import json
+
+brackets_dir = os.path.join(os.path.dirname(__file__), 'fixtures')
+
+def _get_brackets(tax_year):
+  
+    filename = f'tax-brackets--{tax_year}.json'
+    file_with_path = os.path.join(brackets_dir, filename)
+
+    with open(file_with_path) as config_file:
+        json_contents = json.load(config_file)
+        config_file.close()
+
+    return json_contents
+
+
+def test_basic_route(client): 
+  resp = client.get('/tax-calculator/brackets/')
+  brackets_2021 = _get_brackets('2021')
+  assert resp.json == {'tax_brackets': brackets_2021}
